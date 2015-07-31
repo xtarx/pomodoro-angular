5ece4612924a1d3dad92257d0189764665250685
xtarx <xtarx0@gmail.com>
2015-07-31 12:16:12 +0200

    Update read add database

--- README.md
+++ README.md
@@ -7,8 +7,16 @@ Pomodoro technique for time management app powered by angularJS
 ## Demo
 http://pomodoro.shakwa.me/
 
+## Usage
+since the project needs API server, make sure you have WAMP/MAMP/LAMP installed.
+
+- Clone the project into the htdocs/www directory.
+- Import the database.
+- Update the api/config/database.config file with your credentials.
+
 ## Features
 - Easy to use timer.
+- BackEnd API to save current progress and to-do list and provide consistent experince.
 - Integrated with to-do list to keep track of current tasks.
 - Tasks are presisitent in localstorage.
 - Auto resume even if tab refreshed/closed
--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Jul 31, 2015 at 03:55 AM
+-- Generation Time: Jul 31, 2015 at 12:15 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -33,17 +33,7 @@ CREATE TABLE `pomodoros` (
   `info` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   `date` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `pomodoros`
---
-
-INSERT INTO `pomodoros` (`id`, `user_id`, `count`, `info`, `date`, `created_at`) VALUES
-(1, 1, 2, NULL, '2015-07-28', '2015-07-29 18:48:18'),
-(2, 1, 3, NULL, '2015-07-29', '2015-07-29 18:49:29'),
-(3, 1, 2, NULL, '2015-07-30', '2015-07-29 22:24:25'),
-(4, 1, 1, NULL, '2015-07-31', '2015-07-30 22:45:29');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -59,17 +49,7 @@ CREATE TABLE `todos` (
   `add_date` date NOT NULL,
   `done_date` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `todos`
---
-
-INSERT INTO `todos` (`id`, `user_id`, `text`, `done`, `add_date`, `done_date`, `created_at`) VALUES
-(1, 1, 'ninasdad asdasdas', 1, '2015-07-30', '2015-07-31', '2015-07-30 17:42:28'),
-(15, 1, 'amr', 0, '2015-07-31', '0000-00-00', '2015-07-30 22:20:20'),
-(17, 1, 'patrick', 1, '2015-07-31', '2015-07-31', '2015-07-30 22:20:23'),
-(18, 1, 'asd', 0, '2015-07-31', '0000-00-00', '2015-07-30 22:44:50');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -85,14 +65,6 @@ CREATE TABLE `users` (
   `token_date` date NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `token`, `token_date`, `created_at`) VALUES
-(1, 'admin@echohr.com', '123', 'b06d2ca9832c4501b032724136611dd1', '2015-07-30', '2015-07-29 15:35:50'),
-(2, 'amr@echohr.com', '123', '', '0000-00-00', '2015-07-31 01:12:05');
 
 --
 -- Indexes for dumped tables
@@ -124,12 +96,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pomodoros`
 --
 ALTER TABLE `pomodoros`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
